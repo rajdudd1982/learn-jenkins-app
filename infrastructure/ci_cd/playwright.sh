091b6a6a087c1bd1797c76562e19a7365f419d79
rajdudd1982 <58421454+rajdudd1982@users.noreply.github.com>
2025-02-11 23:22:58 +0000

    change port to 3021

--- infrastructure/ci_cd/playwright.sh
+++ infrastructure/ci_cd/playwright.sh
@@ -11,12 +11,12 @@ docker build -f infrastructure/ci_cd/Dockerfile_playwright -t ${imageName} .
 docker rm ${containerName}
 
 #run container
-docker run -d -it  --name ${containerName} ${imageName}
+docker run -d -it -p 3021:3000 --name ${containerName} ${imageName}
 
 
 docker exec -i ${containerName} bash -c 'npm install serve'
 
 #run serve
-docker exec -i ${containerName} bash -c 'npx serve -s /app/build -p 3000'
+docker exec -i ${containerName} bash -c 'PORT=3021 npx serve -s /app/build -p 3021'
 
 docker exec ${containerName} npx playwright test
